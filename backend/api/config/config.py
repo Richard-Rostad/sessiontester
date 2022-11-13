50738c212dd2890b9cb6224c3fff0249b2176717
Richard Rostad <richard.rostad@nav.no>
2022-11-13 17:02:04 +0100

    Actually actually functional backend

--- backend/api/config/config.py
+++ backend/api/config/config.py
@@ -3,12 +3,12 @@ class Config(object):
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI =  'mysql+pymysql://sessions:sessions@localhost:3306/sessiondb'
+    SQLALCHEMY_DATABASE_URI =  'mysql+pymysql://sessions:sessions@host.docker.internal:3306/sessiondb'
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI =  'mysql+pymysql://sessions:sessions@localhost:3306/sessiondb'
+    SQLALCHEMY_DATABASE_URI =  'mysql+pymysql://sessions:sessions@host.docker.internal:3306/sessiondb'
     SQLALCHEMY_ECHO = False
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sessions:sessions@localhost:3306/sessiondb'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sessions:sessions@host.docker.internal:3306/sessiondb'
     SQLALCHEMY_ECHO = False
